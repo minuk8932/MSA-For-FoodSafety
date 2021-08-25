458501b9815c51fa1fd90545a897e596acebf61d
minuk8932 <minuk8932@naver.com>
2021-08-25 12:51:10 +0900

    Feature: add dependencies, Entity nutrients

---
+++ open-api/src/main/java/com/example/models/Nutrients.java
@@ -0,0 +1,60 @@
+package com.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Builder
+@Table(name = "foods", schema = "food_safety")
+public class Nutrients {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "food_name")
+    private String foodName;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "total")
+    private double total;
+
+    @Column(name = "kcal")
+    private double kcal;
+
+    @Column(name = "carbohydrate")
+    private double carbohydrate;
+
+    @Column(name = "protein")
+    private double protein;
+
+    @Column(name = "fat")
+    private double fat;
+
+    @Column(name = "sugar")
+    private double sugar;
+
+    @Column(name = "sodium")
+    private double sodium;
+
+    @Column(name = "cholesterol")
+    private double cholesterol;
+
+    @Column(name = "saturated_fatty_acid")
+    private double saturatedFattyAcid;
+
+    @Column(name = "trans_fat")
+    private double transFat;
+
+}
