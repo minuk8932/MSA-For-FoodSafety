53290bce748505577208f51f1ca634bfea07f6f2
minuk8932 <minuk8932@naver.com>
2021-09-04 18:13:46 +0900

    Refactor: Method post -> get

--- recipe-api/src/main/java/com/example/controller/RecipeController.java
+++ recipe-api/src/main/java/com/example/controller/RecipeController.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +31,7 @@ public class RecipeController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JdbcTemplate.class);
 
-    @PostMapping("/recommend")
+    @GetMapping("/recommend")
     public ResponseEntity<List<Recipes>> menuRecommender(@RequestBody Map<String, Integer> ate) {
         List<Recipes> recommend = null;
 
