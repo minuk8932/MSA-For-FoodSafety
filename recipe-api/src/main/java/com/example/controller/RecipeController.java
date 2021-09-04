5543694c88846691b774de307eb166b8d1f5c025
minuk8932 <minuk8932@naver.com>
2021-09-04 17:59:36 +0900

    Feature: RecipeController

---
+++ recipe-api/src/main/java/com/example/controller/RecipeController.java
@@ -0,0 +1,53 @@
+package com.example.controller;
+
+import com.example.model.Recipes;
+import com.example.service.NutrientElementsServiceImpl;
+import com.example.service.OpenApiRemoteServiceImpl;
+import com.example.service.RecipeRecommendServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("recipes")
+public class RecipeController {
+
+    private final RecipeRecommendServiceImpl recipeRecommendService;
+
+    private final NutrientElementsServiceImpl nutrientElementsService;
+
+    private final OpenApiRemoteServiceImpl openApiRemoteService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    @PostMapping("/recommend")
+    public ResponseEntity<List<Recipes>> menuRecommender(@RequestBody Map<String, Integer> ate) {
+        List<Recipes> recommend = null;
+
+        try {
+
+            List<Recipes> remoteDataList = (List<Recipes>) openApiRemoteService.getOpenApiDataList();
+            double[] ingested = nutrientElementsService.ingestedNutrientsAvg(ate);
+
+            recommend = (List<Recipes>) recipeRecommendService.menuRecommender(remoteDataList, ingested);
+
+        } catch (NullPointerException nullPointerException) {
+            LOGGER.error(">>> RecipeController >> exception >> ", nullPointerException, " >> wrong recipe list !!");
+            nullPointerException.printStackTrace();
+        }
+
+        return new ResponseEntity<>(recommend, HttpStatus.OK);
+    }
+
+}
