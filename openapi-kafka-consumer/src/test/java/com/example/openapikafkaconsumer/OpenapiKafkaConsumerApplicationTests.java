12a67ec390d3fec785d41eab345eea8d302e4203
minuk8932 <minuk8932@naver.com>
2022-01-17 14:21:08 +0900

    Feat: add Kafka consumer

---
+++ openapi-kafka-consumer/src/test/java/com/example/openapikafkaconsumer/OpenapiKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapikafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
