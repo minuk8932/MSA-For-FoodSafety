a2cc4ab9d67513075e89d0643c482423ff00ca9b
minuk8932 <minuk8932@naver.com>
2022-01-19 22:11:05 +0900

    Feat: add KafkaTemplate

--- open-api-recipe/src/main/java/com/example/controller/OpenApiRecipeController.java
+++ open-api-recipe/src/main/java/com/example/controller/OpenApiRecipeController.java
@@ -5,6 +5,7 @@ import com.example.service.OpenApiServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 public class OpenApiRecipeController {
 
     private final OpenApiServiceImpl openApiService;
+    private final KafkaTemplate<String, Recipes> kafkaTemplate;
 
     @GetMapping(path = "/all")
     public ResponseEntity<List<Recipes>> getAllRecipes() {
